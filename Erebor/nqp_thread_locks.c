f4255e6b6fe06beeae815bdcec12a0bbfb9bc335
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-23 18:28:43 -0500

    Locking -- Work's Started

--- Erebor/nqp_thread_locks.c
+++ Erebor/nqp_thread_locks.c
@@ -1,35 +1,40 @@
 #include <stdatomic.h>
 #include "nqp_thread_locks.h"
 
-nqp_mutex_t *nqp_thread_mutex_init( void )
+/**
+ * Initialize an nqp_mutex_t.
+ *
+ * Return: An initialized lock or NULL on error.
+ */
+nqp_mutex_t *nqp_thread_mutex_init(void)
 {
     return NULL;
 }
 
-int nqp_thread_mutex_lock( nqp_mutex_t *mutex )
+int nqp_thread_mutex_lock(nqp_mutex_t *mutex)
 {
-    (void) mutex;
+    (void)mutex;
 
     return -1;
 }
 
-int nqp_thread_mutex_trylock( nqp_mutex_t *mutex )
+int nqp_thread_mutex_trylock(nqp_mutex_t *mutex)
 {
-    (void) mutex;
+    (void)mutex;
 
     return -1;
 }
 
-int nqp_thread_mutex_unlock( nqp_mutex_t *mutex )
+int nqp_thread_mutex_unlock(nqp_mutex_t *mutex)
 {
-    (void) mutex;
+    (void)mutex;
 
     return -1;
 }
 
-int nqp_thread_mutex_destroy( nqp_mutex_t *mutex )
+int nqp_thread_mutex_destroy(nqp_mutex_t *mutex)
 {
-    (void) mutex;
+    (void)mutex;
 
     return -1;
 }
