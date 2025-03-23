c78f7f945c0d6ecb508ac877ab394950086f794f
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-23 18:29:50 -0500

    Specific Instructions Addedcd

--- Erebor/nqp_thread_locks.c
+++ Erebor/nqp_thread_locks.c
@@ -1,6 +1,31 @@
 #include <stdatomic.h>
 #include "nqp_thread_locks.h"
 
+// Assignment Instructions :
+/*
+
+In this part you will implement your own locks as defined in the interface in nqp_thread_locks.h.
+
+Lock implementations require the use of atomic instructions and/or data types.
+You can and should use the atomic instructions and/or data types
+that are defined in stdatomic.h to implement your locks.
+
+You should implement your locks as spin locks using the atomic primitives in stdatomic.h.
+
+You should evaluate your implementation of locks for correctness (e.g., enforcing mutual
+exclusion of two concurrent threads of execution) by using them in a program with pthreads.
+ You are permitted to copy one of the concurrent code samples from class (e.g., the bad adding
+  code) and modify the code to use your lock interface.
+
+As you continue your implementation of concurrency: when a lock cannot be acquired by a thread
+ it should yield (i.e., call nqp_yield as defined in nqp_thread_sched.h). Your implementation
+  may still spin prior to yielding, but spinning should not be “infinite” (your thread must
+  eventually give up control because no other thread can release the lock until your thread
+  has given up control).
+
+
+
+*/
 /**
  * Initialize an nqp_mutex_t.
  *
