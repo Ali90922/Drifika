6e8817827a5567aff385e208e7bd963ae8f72755
Ali90922 <nawaza1@myumanitoba.ca>
2025-03-25 14:51:06 -0500

    Safe Code and Error cHECKING

--- Erebor/nqp_thread_locks.c
+++ Erebor/nqp_thread_locks.c
@@ -63,9 +63,9 @@ nqp_mutex_t *nqp_thread_mutex_init(void)
 
 int nqp_thread_mutex_lock(nqp_mutex_t *mutex)
 {
-    (void)mutex;
-
-    return -1;
+    if (!mutex) {
+        return -1; // Error: NULL pointer provided.
+    }
 }
 
 
