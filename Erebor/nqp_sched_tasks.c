09f6158bd347db7045b7ee86c075718fcbe4a775
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-25 18:00:35 -0500

    Supressing the warnings from my clang compiler

--- Erebor/nqp_sched_tasks.c
+++ Erebor/nqp_sched_tasks.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
+#include <unistd.h> // Make sure to include this for write(), usleep()
 #include "nqp_thread.h"
 #include "nqp_thread_sched.h"
 
@@ -23,86 +24,85 @@ typedef struct THREAD
     thread_type type;
 } thread;
 
-useconds_t work_time[] = 
-{
-    [INTERACTIVE] = 1000,
-    [SEMI_INTERACTIVE] = 4000,
-    [NON_INTERACTIVE] = 10000,
+useconds_t work_time[] =
+    {
+        [INTERACTIVE] = 1000,
+        [SEMI_INTERACTIVE] = 4000,
+        [NON_INTERACTIVE] = 10000,
 };
 
 char work_symbol[] =
-{
-    [INTERACTIVE] = 'i',
-    [SEMI_INTERACTIVE] = 's',
-    [NON_INTERACTIVE] = 'n',
+    {
+        [INTERACTIVE] = 'i',
+        [SEMI_INTERACTIVE] = 's',
+        [NON_INTERACTIVE] = 'n',
 };
 
-void do_work( void *arg )
+void do_work(void *arg)
 {
-    thread *t = (thread *) arg;
-    assert( t != NULL );
-    assert( t->type >= 0 && t->type < THREAD_TYPES );
+    thread *t = (thread *)arg;
+    assert(t != NULL);
+    assert(t->type >= 0 && t->type < THREAD_TYPES);
 
-    while( t->progress < MAX_PROGRESS )
+    while (t->progress < MAX_PROGRESS)
     {
         t->progress++;
-        write( STDOUT_FILENO, &work_symbol[ t->type ], 1 );
-        usleep( work_time[ t->type ] );
+        (void)write(STDOUT_FILENO, &work_symbol[t->type], 1);
+        usleep(work_time[t->type]);
         nqp_yield();
     }
     nqp_exit();
 }
 
-
-int main( void )
+int main(void)
 {
     thread threads[THREADS] = {0};
     nqp_thread_t *nqp_threads[THREADS] = {0};
 
-    for ( int i = 0; i < THREADS; i++ )
+    for (int i = 0; i < THREADS; i++)
     {
         threads[i].progress = 0;
         // one third will be "interactive", one third "semi-interactive", and
         // one third "noninteractive".
-        if ( i < THREADS / 3 )
+        if (i < THREADS / 3)
         {
-            write( STDOUT_FILENO, &work_symbol[ INTERACTIVE ], 1 );
+            (void)write(STDOUT_FILENO, &work_symbol[INTERACTIVE], 1);
             threads[i].type = INTERACTIVE;
         }
-        else if ( i >= THREADS / 3 && i < ( 2 * THREADS / 3 ) )
+        else if (i >= THREADS / 3 && i < (2 * THREADS / 3))
         {
-            write( STDOUT_FILENO, &work_symbol[ SEMI_INTERACTIVE ], 1 );
+            (void)write(STDOUT_FILENO, &work_symbol[SEMI_INTERACTIVE], 1);
             threads[i].type = SEMI_INTERACTIVE;
         }
         else
         {
-            write( STDOUT_FILENO, &work_symbol[ NON_INTERACTIVE ], 1 );
+            (void)write(STDOUT_FILENO, &work_symbol[NON_INTERACTIVE], 1);
             threads[i].type = NON_INTERACTIVE;
         }
 
-        nqp_threads[i] = nqp_thread_create( do_work, &threads[i] );
+        nqp_threads[i] = nqp_thread_create(do_work, &threads[i]);
     }
 
-    write( STDOUT_FILENO, "\n", 1 );
+    (void)write(STDOUT_FILENO, "\n", 1);
 
     nqp_sp_settings settings = {0};
     settings.mlfq_settings.queue_time_allotment = 125000;
     settings.mlfq_settings.boost_time = 2000000;
 
-    nqp_sched_init( NQP_SP_MLFQ, &settings );
+    nqp_sched_init(NQP_SP_MLFQ, &settings);
 
     // you can uncomment these to see how the behaviour of threads changes with
     // the scheduling policies you've implemented:
-//    nqp_sched_init( NQP_SP_RR, NULL );
-//    nqp_sched_init( NQP_SP_FIFO, NULL );
-    nqp_sched_start( );
+    //    nqp_sched_init( NQP_SP_RR, NULL );
+    //    nqp_sched_init( NQP_SP_FIFO, NULL );
+    nqp_sched_start();
 
-    for ( int i = 0; i < THREADS; i++ )
+    for (int i = 0; i < THREADS; i++)
     {
-        nqp_thread_join( nqp_threads[i] );
+        nqp_thread_join(nqp_threads[i]);
     }
 
-    write( STDOUT_FILENO, "\nAll done!\n", 11 );
+    (void)write(STDOUT_FILENO, "\nAll done!\n", 11);
 
     return EXIT_SUCCESS;
 }
