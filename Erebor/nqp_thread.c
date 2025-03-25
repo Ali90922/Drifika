ac3f7093d83ad95ac31ab4628c8647e37f6c32bd
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-25 15:59:35 -0500

    TCB

--- Erebor/nqp_thread.c
+++ Erebor/nqp_thread.c
@@ -4,44 +4,47 @@
 #include "nqp_thread.h"
 #include "nqp_thread_sched.h"
 
+typedef struct thread_control_block
+{
+    ucontext_t context;
+    void *stack;
+    // Optionally, add thread ID, status, priority, etc.
+} tcb_t;
+
 static nqp_scheduling_policy system_policy = NQP_SP_TWOTHREADS;
 
-nqp_thread_t *nqp_thread_create( void (*task)(void *), void *arg )
+nqp_thread_t *nqp_thread_create(void (*task)(void *), void *arg)
 {
-    assert( task != NULL );
-    (void) arg;
+    assert(task != NULL);
+    (void)arg;
 
-    if ( task != NULL )
+    if (task != NULL)
     {
-
     }
 
     return NULL;
 }
 
-int nqp_thread_join( nqp_thread_t *thread )
+int nqp_thread_join(nqp_thread_t *thread)
 {
-    assert( thread != NULL );
+    assert(thread != NULL);
 
-    if ( thread != NULL )
+    if (thread != NULL)
     {
-
     }
-    
+
     return -1;
 }
 
-
-
-int nqp_sched_init( const nqp_scheduling_policy policy,
-                    const nqp_sp_settings *settings )
+int nqp_sched_init(const nqp_scheduling_policy policy,
+                   const nqp_sp_settings *settings)
 {
     int ret = -1;
-    (void) settings;
+    (void)settings;
 
-    assert( policy >= NQP_SP_TWOTHREADS && policy < NQP_SP_POLICIES );
+    assert(policy >= NQP_SP_TWOTHREADS && policy < NQP_SP_POLICIES);
 
-    if ( policy >= NQP_SP_TWOTHREADS && policy < NQP_SP_POLICIES )
+    if (policy >= NQP_SP_TWOTHREADS && policy < NQP_SP_POLICIES)
     {
         system_policy = policy;
         ret = 0;
@@ -49,17 +52,17 @@ int nqp_sched_init( const nqp_scheduling_policy policy,
     return ret;
 }
 
-void nqp_yield( void )
+void nqp_yield(void)
 {
     // schedule another (maybe different) task.
 }
 
-void nqp_exit( void )
+void nqp_exit(void)
 {
     // remove the currently executing thread from the system.
 }
 
-void nqp_sched_start( void )
+void nqp_sched_start(void)
 {
     // start scheduling tasks.
 }
