bb7da13dd3453bd00b3ed2fe5f852aa0d15192fa
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-26 14:15:15 -0500

    Function # 2

--- Erebor/nqp_thread.c
+++ Erebor/nqp_thread.c
@@ -71,6 +71,16 @@ nqp_thread_t *nqp_thread_create(void (*task)(void *), void *arg)
     return new_thread;
 }
 
+/**
+ * Wait for the specified thread to finish. This function will block the caller
+ * until the specified thread is finished its current task.
+ *
+ * Args:
+ *  thread: the thread to wait for. Must not be NULL. Must have been previously
+ *          initialized.
+ * Return: 0 on success (e.g., the thread has exited), -1 on failure.
+ */
+
 int nqp_thread_join(nqp_thread_t *thread)
 {
     assert(thread != NULL);
