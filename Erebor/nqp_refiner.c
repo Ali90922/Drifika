efdcda4080a93e4ace6fee2c3c939d34f67fefab
Ali Nawaz <150611589+Ali90922@users.noreply.github.com>
2025-03-28 17:16:51 -0500

    Updated

--- Erebor/nqp_refiner.c
+++ Erebor/nqp_refiner.c
@@ -6,34 +6,34 @@
 #include "nqp_thread.h"
 #include "nqp_thread_sched.h"
 
-#define THREADS 40
+#define THREADS 20
 #define MAX_PROGRESS 30
 #define MAX_WORK_UNIT 3
 
 uint8_t progress[THREADS] = {0};
 
-bool all_full( void )
+bool all_full(void)
 {
     bool full = true;
 
-    for ( int i = 0; i < THREADS; i++ )
+    for (int i = 0; i < THREADS; i++)
     {
         full &= progress[i] >= MAX_PROGRESS;
     }
     return full;
 }
-void refine_macrodata( void *arg )
+void refine_macrodata(void *arg)
 {
-    assert( arg != NULL );
-    size_t my_bar = *(int *) arg;
-    assert( my_bar >= 0 );
-    assert( my_bar < THREADS );
+    assert(arg != NULL);
+    size_t my_bar = *(int *)arg;
+    assert(my_bar >= 0);
+    assert(my_bar < THREADS);
 
-    while ( progress[my_bar] < MAX_PROGRESS )
+    while (progress[my_bar] < MAX_PROGRESS)
     {
         progress[my_bar] += rand() % MAX_WORK_UNIT;
 
-        if ( progress[my_bar] > MAX_PROGRESS )
+        if (progress[my_bar] > MAX_PROGRESS)
         {
             progress[my_bar] = MAX_PROGRESS;
         }
@@ -42,15 +42,15 @@ void refine_macrodata( void *arg )
     nqp_exit();
 }
 
-void display_progress( void )
+void display_progress(void)
 {
     // clear the screen
     printf("\033[2J");
 
-    for ( int i = 0; i < THREADS; i++ )
+    for (int i = 0; i < THREADS; i++)
     {
-        printf("Refiner %02d: ", i );
-        if ( progress[i] >= MAX_PROGRESS )
+        printf("Refiner %02d: ", i);
+        if (progress[i] >= MAX_PROGRESS)
         {
             // green checkmark
             printf("\U00002705 ");
@@ -60,7 +60,7 @@ void display_progress( void )
             // hourglass with flowing sand
             printf("\U000023F3 ");
         }
-        for ( int j = 0; j < progress[i]; j++ )
+        for (int j = 0; j < progress[i]; j++)
         {
             putchar('|');
         }
@@ -68,42 +68,42 @@ void display_progress( void )
     }
 }
 
-void grapher( void *arg )
+void grapher(void *arg)
 {
-    (void) arg;
+    (void)arg;
     // print a graph until all the bars are full.
-    while( ! all_full( ) )
+    while (!all_full())
     {
-        display_progress( );
-        usleep( 125000 );
-        nqp_yield( );
+        display_progress();
+        usleep(125000);
+        nqp_yield();
     }
     // display final progress where all the macrodata is refined.
-    display_progress( );
-    nqp_exit( );
+    display_progress();
+    nqp_exit();
 }
 
-int main( void )
+int main(void)
 {
     int work_unit[THREADS];
-    nqp_thread_t *display = nqp_thread_create( grapher, NULL );
+    nqp_thread_t *display = nqp_thread_create(grapher, NULL);
     nqp_thread_t *macrodata_refiners[THREADS] = {0};
 
-    for ( int i = 0; i < THREADS; i++ )
+    for (int i = 0; i < THREADS; i++)
     {
         work_unit[i] = i;
-        macrodata_refiners[i] = nqp_thread_create( refine_macrodata, &work_unit[i] );
+        macrodata_refiners[i] = nqp_thread_create(refine_macrodata, &work_unit[i]);
     }
 
     // this can't work with FIFO, all threads need to be able to make progress
     // for the display to work.
-    nqp_sched_init( NQP_SP_RR, NULL );
-    nqp_sched_start( );
+    nqp_sched_init(NQP_SP_RR, NULL);
+    nqp_sched_start();
 
-    nqp_thread_join( display );
-    for ( int i = 0; i < THREADS; i++ )
+    nqp_thread_join(display);
+    for (int i = 0; i < THREADS; i++)
     {
-        nqp_thread_join( macrodata_refiners[i] );
+        nqp_thread_join(macrodata_refiners[i]);
     }
 
     printf("All done!\n");
